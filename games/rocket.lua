9acd7fb75b3f8d2f4b1708f8afce846446244d93
Mees Brinkhuis <m.brinkhuis@aquablu.com>
2025-06-07 22:25:10 +0200

    rocket game works well now

--- games/rocket.lua
+++ games/rocket.lua
@@ -1,14 +1,22 @@
 
 log("testing rocket game")
 
--- set physics
-x = 64
-y = 64
+-- set variables
+x = 0
+y = 0
 dy = 0
 dx = 0
 r = 0
-
 stars = {}
+points = 0
+m_timer = 0
+m_x = 0
+m_y = 0
+m_dx = 0
+m_dy = 0
+running = true
+
+text(255,255,255,255)
 
 for i=1,30 do 
     star = {
@@ -18,20 +26,29 @@ for i=1,30 do
     table.insert(stars, star)
 end
 
-text(255,255,255,255)
+function reset()
+    x = 64
+    y = 99
+    dy = 0
+    dx = 0
+    r = 0
+    points = -1
+    m_x = -100
+    m_y = -100
+    m_dx = 0
+    m_dy = 0
+    running = true
+    m_timer = millis()
+end
 
-running = true
+reset()
+fill(0, 0, 0, 150)
 
 function _draw()
 
     if not running then
-        if btn(UP) then
-            running = true
-            x = 64
-            y = 64
-            dy = 0
-            dx = 0
-            r = 0
+        if btnp(UP) then
+            reset()
         end
         return
     end
@@ -42,7 +59,7 @@ function _draw()
     dy = dy + 0.01
 
     -- in the air
-    if y < 90 and running then
+    if y < 99 and running then
 
         -- control
         if btn(LEFT) then
@@ -72,6 +89,8 @@ function _draw()
     -- physics
     y = y + dy
     x = x + dx
+    m_x = m_x + m_dx
+    m_y = m_y + m_dy
 
     -- wrap
     if x < -10 then
@@ -94,6 +113,9 @@ function _draw()
         line(star.x, star.y, star.x, star.y)
     end
 
+    -- draw meteor
+    sprite(14, 20, 22, 20, m_x-11, m_y-10, 22, 20, (millis() * 0.1) % 360)
+
     -- draw planet
     sprite(0, 106, 128, 22, 0, 106, 128, 22)
 
@@ -102,27 +124,44 @@ function _draw()
     -- draw rocket
     if btn(UP) and running then
         if(millis() % 100 < 50) then
-            sprite(52, 23, 12, 26, x-6, y, 12, 26, r)
+            sprite(52, 23, 12, 26, x-6, y-9, 12, 26, r)
         else
-            sprite(66, 23, 12, 26, x-6, y, 12, 26, r)
+            sprite(66, 23, 12, 26, x-6, y-9, 12, 26, r)
         end
     else
-        sprite(80, 23, 12, 26, x-6, y, 12, 26, r)
+        sprite(80, 23, 12, 26, x-6, y-9, 12, 26, r)
     end
 
     -- collision with planet
-    if y >= 90 and (dy > 0.3 or math.abs(dx) > 0.3 or math.abs(r) > 18) then
+    if (y >= 99 and (dy > 0.3 or math.abs(dx) > 0.3 or math.abs(r) > 18) or 
+        (math.sqrt(math.abs(x-m_x)*math.abs(x-m_x) + math.abs(y-m_y)*math.abs(y-m_y)) < 17)) then
         running = false
         log("game over")
-        sprite(97, 15, 23, 24, x-11, y, 23, 24)
+        sprite(97, 15, 23, 24, x-11, y-13, 23, 24)
         cursor(64 - (10*4)/2, 60)
-        text(255,255,255,255)
         print("game over")
     end
 
-    cursor(4, 114)
-    fill(0, 0, 0, 200)
+    -- draw points
+    cursor(4, 122)
     stroke(255, 255, 255, 255, 255)
-    print("x:".. math.floor(x) .. "\ny:" .. math.floor(y))
+    if points > 0 then
+        print("points:" .. points)
+    end
+
+    -- spawn a meter every 10 seconds
+    if millis() - m_timer > 5000 then
+        m_dx = math.random(-1, 1) * 0.7
+        m_dy = math.random(-1, 1) * 0.7
+        if m_dx == 0 and m_dy == 0 then
+            m_dx = 0.7
+            m_dy = 0.7
+        end
+        m_x = 64 - m_dx * 150
+        m_y = 64 - m_dy * 150
+        m_timer = millis()
+        log("meteor spawned at: " .. m_x .. ", " .. m_y)
+        points = points + 1
+    end
 
 end
